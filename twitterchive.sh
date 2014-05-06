82de18734440e5ffe844c8dac5cc4fa6b82a17c1
Stephen Turner <vustephen@gmail.com>
2014-05-06 14:28:58 -0400

    Update search results: Tue May  6 14:28:58 EDT 2014

--- twitterchive.sh
+++ twitterchive.sh
@@ -17,7 +17,7 @@
 
 ## declare an array variable containing all your search terms. 
 ## prefix any hashtags with a \
-declare -a arr=(bioinformatics metagenomics genomics rna-seq \#rstats \#cville \#bog14)
+declare -a arr=(\#bog14 bioinformatics metagenomics genomics rna-seq \#rstats \#cville)
 
 ## How many results would you like for each query?
 n=400
